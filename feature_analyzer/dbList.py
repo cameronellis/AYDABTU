6ca64724ef4329d2e4b5f3dee4cfde87331fe155
Nathaniel Moreno <ngmoreno@ucsd.edu>
2015-11-19 18:19:13 -0800

    added dropdowns to GUI

---
+++ feature_analyzer/dbList.py
@@ -0,0 +1,12 @@
+dbases = {
+	'cdc'			: ['national_divorce_trends','national_marriage_trends'],
+	'crime_data'		: ['murders_by_weapon_type','police_military_purchases', 'portland_crime', 'san_diego_crime','us_crime_rates'],
+	'health'		: ['country_death_rates','life_expectancy_hivaids_countries', 'life_expectancy_russia','life_expectancy_us', 'life_expectancy_world_projection_2100','world_population_projection_2100'],
+	'housing_data'		: ['san_francisco_rental_listings','freddie_mae_house_price_index'],
+	'homeland_security'	: ['refugee_arrivals_by_country', 'refugee_arrivals_by_region'],
+	'national_fire_protection': ['fire_problem_overview', 'number_fires_by_type'],
+	'transportation'	: ['aircraft_accident_rates', 'alcohol_related_crashes','monthly_number_of_airline_passengers_all_carriers','retail_deisel_prices', 'unruly_airline_passengers','us_airline_fuel_cost_consumption'],
+	'treasury'		: ['us_debt'],
+	'weather'		: ['tornado_counts','tornado_deaths'],
+	'world_health_organization': ['hepb_immunizations_oneyearolds','infants_exclusivley_bresatfed_first6mos']
+	}
--- feature_analyzer/gui.py
+++ feature_analyzer/gui.py
@@ -1,9 +1,10 @@
 from Tkinter import *
+import dbList
 
 def show_entry_fields():
 	#print("Database Name: %s\nTable Name: %s\nField Name 1: %s\nField name 2: %s" % (e4.get(), e1.get(), e2.get(), e3.get()))
 	fo = open("query.json", "wb")
-	fo.write("{\n\t" + '"'+ e4.get() + '":{\n\t\t' + '"' + e1.get() +'":["' + e2.get()+'","'+e3.get()+'"]\n\t}\n}\n');
+	fo.write("{\n\t" + '"'+ dbSel.get() + '":{\n\t\t' + '"' + tableSel.get() +'":["' + e3.get()+'","'+e4.get()+'"]\n\t}\n}\n');
 	fo.close();
 	e1.delete(0,END)
 	e2.delete(0,END)
@@ -11,28 +12,56 @@ def show_entry_fields():
 	e4.delete(0,END)
 	master.quit()
 
-master = Tk()
-Label(master, text="Table Name").grid(row=0)
-Label(master, text="Field Name 1").grid(row=1)
-Label(master, text="Field Name 2").grid(row=2)
-Label(master, text="Database Name").grid(row=3)
+def option_changed(a,b,c):
+	menu = e2.children["menu"]
+	menu.delete(0, "end")
+	for table in dbList.dbases[dbSel.get()]:
+		menu.add_command(label=table, command=lambda value=table: tableSel.set(value))
 
-e1 = Entry(master)
-e2 = Entry(master)
+
+master = Tk()
+
+dbs = []
+for key in dbList.dbases.keys(): dbs.append(key)	
+dbSel = StringVar(master)
+dbSel.trace("w", option_changed)
+
+tables = ['MUST SELECT DATABASE']
+tableSel = StringVar(master)
+#tableSel.trace("w", table_choice)
+
+Label(master, text="Database Name").grid(row=0)
+Label(master, text="Table Name").grid(row=1)
+Label(master, text="Field Name 1").grid(row=2)
+Label(master, text="Field Name 2").grid(row=3)
+
+e1 = OptionMenu(master,dbSel,*dbs)
+e2 = OptionMenu(master,tableSel,*tables)
 e3 = Entry(master)
 e4 = Entry(master)
 
-e4.insert(10, "national_fire_protection.fire_problem_overview")
-e1.insert(10,"names")
-e2.insert(10,"Civilian Deaths")
-e3.insert(10, "Firefighter deaths")
+#e1.insert(10, "national_fire_protection.fire_problem_overview")
+#e2.insert(10,"names")
+e3.insert(10,"Civilian Deaths")
+e4.insert(10,"Firefighter deaths")
+
+
+
+e1.config(width=20)
+e2.config(width=20)
 
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 e3.grid(row=2, column=1)
 e4.grid(row=3, column=1)
 
+	
+
+
 #Button(master, text='Quit', command=master.quit).grid(row=4, column=0, sticky=W, pady=4)
-Button(master, text='Generate JSON file', command=show_entry_fields).grid(row=4, column=0, sticky=W, pady=4)
+Button(master, text='Generate JSON file', command=show_entry_fields).grid(row=5, column=0, sticky=W, pady=4)
 
 mainloop( )
+
+
+
