7f0682b582d7081d370b14f027abef84f43b67d0
Nathaniel Moreno <ngmoreno@ucsd.edu>
2015-11-19 21:31:48 -0800

    all gui dropdowns work now

--- feature_analyzer/dbList.py
+++ feature_analyzer/dbList.py
@@ -1,12 +1,53 @@
 dbases = {
-	'cdc'			: ['national_divorce_trends','national_marriage_trends'],
-	'crime_data'		: ['murders_by_weapon_type','police_military_purchases', 'portland_crime', 'san_diego_crime','us_crime_rates'],
-	'health'		: ['country_death_rates','life_expectancy_hivaids_countries', 'life_expectancy_russia','life_expectancy_us', 'life_expectancy_world_projection_2100','world_population_projection_2100'],
-	'housing_data'		: ['san_francisco_rental_listings','freddie_mae_house_price_index'],
-	'homeland_security'	: ['refugee_arrivals_by_country', 'refugee_arrivals_by_region'],
-	'national_fire_protection': ['fire_problem_overview', 'number_fires_by_type'],
-	'transportation'	: ['aircraft_accident_rates', 'alcohol_related_crashes','monthly_number_of_airline_passengers_all_carriers','retail_deisel_prices', 'unruly_airline_passengers','us_airline_fuel_cost_consumption'],
-	'treasury'		: ['us_debt'],
-	'weather'		: ['tornado_counts','tornado_deaths'],
-	'world_health_organization': ['hepb_immunizations_oneyearolds','infants_exclusivley_bresatfed_first6mos']
-	}
+	'cdc'			: {
+					'national_divorce_trends':["Year","# Divorces & Annulments","Total Population","Rate per 1000"],
+					'national_marriage_trends':["Year","# Marriages","Total Population","Rate per 1000"]
+					},
+	'crime_data'		: {
+					'murders_by_weapon_type':["Year","Total Murders","Total by Firearms","Handguns","Rifles","Shotguns","Other guns","Firearms", "type not stated","Knives","Blunt objects","Personal weapons","Poison","Explosives","Fire","Narcotics","Drowning","Strangulation","Asphyxiation","Other"],
+					'police_military_purchases': ["Ship Date","Niin Item Name","State","Quantity","Total Cost"],
+					'portland_crime':["Record ID","Report Time","Major Offense Type","Address","Neighborhood","Police Precinct","Police District","X Coordinate","Y Coordinate"],
+					'san_diego_crime':["date","time","type","address","city","lat","lon","desc"],
+					'us_crime_rates':["Year","Violent Crime","Violent Crime Rate","Murder and Non-Negligent Manslaughter","Murder and Non-Negligent Manslaughter Rate","Forcible Rape","Forcible Rape Rate","Robbery","Robbery Rate","Aggravated Assault","Aggravated Assault Rate","Property Crime","Property Crime Rate","Burglary","Burglary Rate","Larceny-Theft","Larceny-Theft Rate","Motor Vehicle Theft","Motor Vehicle Theft Rate","Population"]
+					},
+	'health'		: {
+					'country_death_rates':[],
+					'life_expectancy_hivaids_countries':[],
+					'life_expectancy_russia':[],
+					'life_expectancy_us':[],
+					'life_expectancy_world_projection_2100':[],
+					'world_population_projection_2100':[]
+					},
+	'housing_data'		: {
+					'san_francisco_rental_listings':[],
+					'freddie_mae_house_price_index':[]
+					},
+	'homeland_security'	: {
+					'refugee_arrivals_by_country':["Date","Total","Afghanistan","Algeria","Angola","Armenia","Azerbaijan","Belarus","Bhutan","Bosnia-Herzegovina","Burma","Burundi","Cambodia","Cameroon","Central African Republic","Chad","China","Colombia","Rwanda","Serbia and Montenegro","Sierra Leone","Somalia","Soviet Union, former","Sudan","Syria","Tajikistan","Togo","Tunisia","Turkmenistan","Uganda","Ukraine","Uzbekistan","Vietnam","Yemen","Yugoslavia, former","Zimbabwe","All Other Countries"],
+					'refugee_arrivals_by_region':["Date","Total","Africa","Asia","Europe","North America","Oceania","South America"]
+					},
+	'national_fire_protection': {
+					'fire_problem_overview':[],
+					'number_fires_by_type':[]
+					},
+	'transportation'	: {
+					'aircraft_accident_rates':[],
+					'alcohol_related_crashes':[],
+					'monthly_number_of_airline_passengers_all_carriers':[],
+					'retail_deisel_prices':[],
+					'unruly_airline_passengers':["Year","Total"],
+					'us_airline_fuel_cost_consumption':[]
+					},
+	'treasury'		: {
+					'us_debt':["Year","Debt Held By Public","Intergovernmental Holdings","Total Public Debt Outstanding"],
+					},
+	'weather'		: {
+					'tornado_counts':[],
+					'tornado_deaths':[]
+					},
+
+	'world_health_organization': {
+					'hepb_immunizations_oneyearolds':["Country","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991","1990","1989"],
+					'infants_exclusivley_bresatfed_first6mos':["Country","Year","Precent of Infants"]
+					},
+}
--- feature_analyzer/gui.py
+++ feature_analyzer/gui.py
@@ -4,31 +4,43 @@ import dbList
 def show_entry_fields():
 	#print("Database Name: %s\nTable Name: %s\nField Name 1: %s\nField name 2: %s" % (e4.get(), e1.get(), e2.get(), e3.get()))
 	fo = open("query.json", "wb")
-	fo.write("{\n\t" + '"'+ dbSel.get() + '":{\n\t\t' + '"' + tableSel.get() +'":["' + e3.get()+'","'+e4.get()+'"]\n\t}\n}\n');
+	fo.write("{\n\t" + '"'+ dbSel.get() + '":{\n\t\t' + '"' + tableSel.get() +'":["' + fSel1.get()+'","'+fSel2.get()+'"]\n\t}\n}\n');
 	fo.close();
-	e1.delete(0,END)
-	e2.delete(0,END)
-	e3.delete(0,END)
-	e4.delete(0,END)
+	for f in fieldList:
+		f.children["menu"].delete(0,END)
 	master.quit()
 
 def option_changed(a,b,c):
 	menu = e2.children["menu"]
 	menu.delete(0, "end")
-	for table in dbList.dbases[dbSel.get()]:
+	for table in (dbList.dbases[dbSel.get()]).keys():
 		menu.add_command(label=table, command=lambda value=table: tableSel.set(value))
+
+
+def table_choice(a,b,c):
+	menu3 = e3.children["menu"]
+	menu4 = e4.children["menu"]
+	menu3.delete(0, "end")
+	menu4.delete(0, "end")
+	for field in dbList.dbases[dbSel.get()][tableSel.get()]:
+		menu3.add_command(label=field, command=lambda value=field: fSel1.set(value))
+		menu4.add_command(label=field, command=lambda value=field: fSel2.set(value))
 
 
 master = Tk()
 
 dbs = []
 for key in dbList.dbases.keys(): dbs.append(key)	
-dbSel = StringVar(master)
-dbSel.trace("w", option_changed)
-
 tables = ['MUST SELECT DATABASE']
+fields = ['MUST SELECT DATABASE AND TABLE']
+
+dbSel = StringVar(master)
 tableSel = StringVar(master)
-#tableSel.trace("w", table_choice)
+fSel1 = StringVar(master)
+fSel2 = StringVar(master)
+
+dbSel.trace("w", option_changed)
+tableSel.trace("w", table_choice)
 
 Label(master, text="Database Name").grid(row=0)
 Label(master, text="Table Name").grid(row=1)
@@ -37,18 +49,20 @@ Label(master, text="Field Name 2").grid(row=3)
 
 e1 = OptionMenu(master,dbSel,*dbs)
 e2 = OptionMenu(master,tableSel,*tables)
-e3 = Entry(master)
-e4 = Entry(master)
+e3 = OptionMenu(master,fSel1,*fields)
+e4 = OptionMenu(master,fSel2,*fields)
+fieldList = [e1,e2,e3,e4]
 
 #e1.insert(10, "national_fire_protection.fire_problem_overview")
 #e2.insert(10,"names")
-e3.insert(10,"Civilian Deaths")
-e4.insert(10,"Firefighter deaths")
-
+#e3.insert(10,"Civilian Deaths")
+#e4.insert(10,"Firefighter deaths")
 
 
 e1.config(width=20)
 e2.config(width=20)
+e3.config(width=20)
+e4.config(width=20)
 
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
