2d977a4d079f8b0e16d0fbd9df94ca88193337c9
Nathaniel Moreno <ngmoreno@ucsd.edu>
2015-11-20 08:59:02 -0800

    fully populated dbList.py

--- feature_analyzer/dbList.py
+++ feature_analyzer/dbList.py
@@ -11,43 +11,47 @@ dbases = {
 					'us_crime_rates':["Year","Violent Crime","Violent Crime Rate","Murder and Non-Negligent Manslaughter","Murder and Non-Negligent Manslaughter Rate","Forcible Rape","Forcible Rape Rate","Robbery","Robbery Rate","Aggravated Assault","Aggravated Assault Rate","Property Crime","Property Crime Rate","Burglary","Burglary Rate","Larceny-Theft","Larceny-Theft Rate","Motor Vehicle Theft","Motor Vehicle Theft Rate","Population"]
 					},
 	'health'		: {
-					'country_death_rates':[],
-					'life_expectancy_hivaids_countries':[],
-					'life_expectancy_russia':[],
-					'life_expectancy_us':[],
-					'life_expectancy_world_projection_2100':[],
-					'world_population_projection_2100':[]
+					'country_death_rates':["Country Code","Country Name","Year","Age Group","Sex","Number of Deaths","Death Rate Per 100,000"],
+					'life_expectancy_hivaids_countries':["Year","Botswana","Lesotho (Years)","South Africa (Years)","Swaziland (Years)","Zambia (Years)","Zimbabwe (Years)"],
+					'life_expectancy_russia':["Year","Male (Years)","Female (Years)","Both Sexes (Years)"],
+					'life_expectancy_us':["Year","Life Expectancy at Birth (Years)"],
+					'life_expectancy_world_projection_2100':["Year","Africa (Years)","Asia (Years)","Europe (Years)","Latin America (Years)","Northern America* (Years)","Oceania (Years)","World (Years)"],
+					'world_population_projection_2100':["Year","Population (Millions)","Annual Addition (Millions)","Percent Increase (Millions)"]
 					},
 	'housing_data'		: {
-					'san_francisco_rental_listings':[],
-					'freddie_mae_house_price_index':[]
+					'san_francisco_rental_listings':["numBedrooms","price","latitude","longitude"],
+					'freddie_mae_house_price_index':["Date","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 					},
 	'homeland_security'	: {
 					'refugee_arrivals_by_country':["Date","Total","Afghanistan","Algeria","Angola","Armenia","Azerbaijan","Belarus","Bhutan","Bosnia-Herzegovina","Burma","Burundi","Cambodia","Cameroon","Central African Republic","Chad","China","Colombia","Rwanda","Serbia and Montenegro","Sierra Leone","Somalia","Soviet Union, former","Sudan","Syria","Tajikistan","Togo","Tunisia","Turkmenistan","Uganda","Ukraine","Uzbekistan","Vietnam","Yemen","Yugoslavia, former","Zimbabwe","All Other Countries"],
 					'refugee_arrivals_by_region':["Date","Total","Africa","Asia","Europe","North America","Oceania","South America"]
 					},
 	'national_fire_protection': {
-					'fire_problem_overview':[],
-					'number_fires_by_type':[]
+					'fire_problem_overview':["Year","Fires","Civilian deaths","Civilian injuries","Firefighter deaths","Firefighter injuries","Direct property damage (in billions) As reported","Direct property damage (in billions) In 2011 dollars"],
+					'number_fires_by_type':["Year","Total","Structures","Outside of structures with value involved but no vehicle","Highway vehicles","Brush grass wildland  with no value or loss involved","Rubbish including dumpsters with no value or loss involved","All other fires"]
 					},
 	'transportation'	: {
-					'aircraft_accident_rates':[],
-					'alcohol_related_crashes':[],
-					'monthly_number_of_airline_passengers_all_carriers':[],
-					'retail_deisel_prices':[],
+					'aircraft_accident_rates':["Year","Major Accidents","Serious Accidents","Injuries","Damage","Aircraft Hours","Major Accidents per Million Hours","Serious accidents per Million Hours", "Injuries per Million Hours","Damage per Million Hours"],
+					'alcohol_related_crashes':["Year","Traffic Crashes","Traffic Fatalities","Alcohol Related Fatalities","% of All Fatalities"],
+					'monthly_number_of_airline_passengers_all_carriers':["Date","Domestic","International","Total"],
+					'retail_deisel_prices':["Date","Price"],
 					'unruly_airline_passengers':["Year","Total"],
-					'us_airline_fuel_cost_consumption':[]
+					'us_airline_fuel_cost_consumption':["Month","Domestic Consumption (Millions of Gallons)","Domestic Cost (Millions of $)","Domestic Cost per Gallon ($)","International Consumption (Millions of Gallons)","International (Millions of $)","International Cost per Gallon ($)","Total Consumption (Millions of Gallons)","Total (Millions of $)","Total Cost per Gallon ($)"]
 					},
 	'treasury'		: {
 					'us_debt':["Year","Debt Held By Public","Intergovernmental Holdings","Total Public Debt Outstanding"],
 					},
 	'weather'		: {
-					'tornado_counts':[],
-					'tornado_deaths':[]
+					'tornado_counts':["Year","Total # of Tornadoes","Jan","Feb","March","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"],
+					'tornado_deaths':["Year","Total # of Deaths from Tornadoes","Jan","Feb","March","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"],
 					},
 
 	'world_health_organization': {
 					'hepb_immunizations_oneyearolds':["Country","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991","1990","1989"],
 					'infants_exclusivley_bresatfed_first6mos':["Country","Year","Precent of Infants"]
 					},
+
+	'us_census'		: {
+					'us_military_aid_inmillions':["Year","Total Foreign Assistance","Military Assistance","Total","US Agency for Intl Dev","US Dept of Agr","State Dept","Other Agencies","Multilateral Orgs"]
+					}
 }
