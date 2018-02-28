47a7f5532986ce16cd522a6528ce08d5fe3caa2b
Cameron Ellis <cameronellis71@gmail.com>
2018-02-27 16:36:35 -0800

    modified script to work in an environment where DISPLAY is undefined

--- visualization/housingAnalysis/stateCompare.py
+++ visualization/housingAnalysis/stateCompare.py
@@ -1,3 +1,6 @@
+import matplotlib
+# Must be before importing matplotlib.pyplot or pylab!
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np 
 import json
@@ -12,17 +15,19 @@ east = [(data["results"][0]["data"][x][2]+data["results"][0]["data"][x][3])/2   
 west = np.asarray(west)
 east = np.asarray(east)
 
+fig = plt.figure()
 plt.scatter(west, east)
-
 
 xs = [a for a in range (0,100)]
 ys = [b for b in range (0,100)]
+
 plt.plot(xs,ys)
+
 plt.ylim([0,100])
 plt.xlim([0,100])
 
 plt.xlabel("West Coast House Price Index")
 plt.ylabel("East Coast House Price Index")
 
-
-plt.show()
+# Export the table to a PNG image
+fig.savefig('temp.png')
